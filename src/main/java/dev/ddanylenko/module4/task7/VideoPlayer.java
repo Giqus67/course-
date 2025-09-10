3b478fca39dd4529e5101d539c2fab4e4ea89bd8
Giqus67 <danylenkodenys67@gmail.com>
2025-09-10 18:50:55 +0300

    task7 basic playable interface

---
+++ src/main/java/dev/ddanylenko/module4/task7/VideoPlayer.java
@@ -0,0 +1,8 @@
+package dev.ddanylenko.module4.task7;
+
+public class VideoPlayer implements Playable{
+    @Override
+    public void play() {
+        System.out.println("Starting video on player");
+    }
+}
